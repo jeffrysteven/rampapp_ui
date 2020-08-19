fb68788c81c261c6dfa0cf7d1454a24a6b125dc0
Jeffry Lenis <jeffreyleji@gmail.com>
2020-08-19 07:57:45 -0500

    * Adiciona layout para mostrar yaml. (#13)
    * Ajusta un poco el layout de la app.
    
    Co-authored-by: Jeffry <jeffry@Jeffrys-MacBook-Pro.local>

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,16 +1,10 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import { About, Home } from "../pages";
+import { Home } from "../pages";
 
 export const Routes = () => {
   return (
     <Switch>
-      <Route path="/home">
-        <Home />
-      </Route>
-      <Route path="/about">
-        <About />
-      </Route>
       <Route path="/">
         <Home />
       </Route>
