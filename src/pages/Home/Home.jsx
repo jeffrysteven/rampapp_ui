c52f65cf5850e0e08f876cb999c067a951ceead4
Jeffry <jeffry@Jeffrys-MacBook-Pro.local>
2020-08-18 20:12:48 -0500

    * Adiciona layout para mostrar yaml. * Ajusta un poco el layout de la app.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,19 +1,114 @@
-import React, { useState } from 'react';
-import { Button } from '@material-ui/core';
+import React from 'react';
+import { YamlOutput } from "../../components/YamlOutput";
+import { Drawer, AppBar, Toolbar, Typography } from '@material-ui/core';
 import styles from './Home.css';
 
+const containersMock = {
+  version: 3,
+  services: {
+    db: {
+      image: "mongo",
+      restart: "always",
+      ports: [
+        "27017:27017"
+      ],
+      volumes: [
+        "~/mongo/data:/data/db"
+      ],
+      networks: [
+        "services"
+      ]
+    },
+    api: {
+      depends_on: [
+        "db"
+      ],
+      image: "parseplatform/parse-server",
+      restart: "always",
+      ports: [
+        "1337:1337"
+      ],
+      networks: [
+        "services",
+        "gateway"
+      ],
+      environment: [
+        "PARSE_SERVER_APPLICATION_ID",
+        "PARSE_SERVER_MASTER_KEY",
+        "PARSE_SERVER_DATABASE_URI"
+      ]
+    },
+    dashboard: {
+      depends_on: [
+        "api"
+      ],
+      image: "parseplatform/parse-dashboard",
+      restart: "always",
+      ports: [
+        "4040:4040"
+      ],
+      networks: [
+        "gateway"
+      ],
+      environment: [
+        "PARSE_DASHBOARD_SERVER_URL",
+        "PARSE_DASHBOARD_APP_ID",
+        "PARSE_DASHBOARD_MASTER_KEY",
+        "PARSE_DASHBOARD_APP_NAME",
+        "PARSE_DASHBOARD_ALLOW_INSECURE_HTTP",
+        "PARSE_DASHBOARD_USER_ID",
+        "PARSE_DASHBOARD_USER_PASSWORD"
+      ]
+    },
+    rest: {
+      build: ".",
+      restart: "always",
+      depends_on: [
+        "api"
+      ],
+      ports: [
+        "80:3000"
+      ],
+      networks: [
+        "gateway"
+      ],
+      environment: [
+        "PARSE_SERVER_URL"
+      ],
+      volumes: [
+        ".:/opt/app",
+        "/opt/app/node_modules"
+      ]
+    }
+  },
+  networks: {
+    services: {
+      "driver": "bridge",
+      "internal": true
+    },
+    gateway: {
+      "driver": "bridge"
+    }
+  }
+}
+
 export const Home = () => {
-  const [clicks, setClicks] = useState(0);
-
-  const clickHandler = () => {
-    setClicks(clicks + 1);
-  };
-
   return (
-    <div className={styles.home}>
-      <label className="block">You clicked me {clicks} times</label>c
-      <hr/>
-      <Button onClick={clickHandler}>Click me</Button>
+    <div className={styles.root}>
+      <AppBar position="fixed" className={styles.appbar}>
+        <Toolbar>
+          <Typography variant="h6" noWrap>
+            RampApp
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="permanent" anchor="left" className={styles.menubar}>
+        <Toolbar />
+        <div className="drawer-container">PROJECTS HERE</div>
+      </Drawer>
+      <main className={styles.content}>
+        <YamlOutput json={containersMock} />
+      </main>
     </div>
   );
 };
